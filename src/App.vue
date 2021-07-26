24487903c161f838c06afd52f3a0cb5a85db6342
coln clock <jerry@vcampute.com>
2021-07-26 21:21:54 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Header></Header>
+      <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//引入
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
-export default {
+export default { 
   name: 'App',
   components: {
-    HelloWorld
+    // 注册
+    Header,Footer
   }
 }
 </script>
--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -1,10 +1,10 @@
 <template>
-  
+  <div>typeNav</div>
 </template>
 
 <script>
 export default {
-
+    name:"TypeNav"
 }
 </script>
 
